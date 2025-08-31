8f39bd04e59c5eabf245c498d8ffffbad9eaf656
blueberry-adii <adithyap455@gmail.com>
2025-08-31 15:47:24 +0530

    fix: add authMiddleware to make logout endpoint a protected route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", authMiddleware, logout);
 router.get("/me", authMiddleware, me);
 
 module.exports = router;
